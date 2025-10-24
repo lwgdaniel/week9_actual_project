f31130564d468d381beb7ba894e235ba34cbc6cc
lwgdaniel <144545422+lwgdaniel@users.noreply.github.com>
2025-10-24 23:39:49 +0800

    Update main.py

--- main.py
+++ main.py
@@ -112,7 +112,7 @@ if st.sidebar.button("Clear memory and reset"):
     st.session_state.clear()
     st.rerun()
 
-choice = st.radio(
+choice = st.sidebar.radio(
     "Show system(backend) messages?:",
     options=["Yes", "No"],
     index=0  # 0 = 'Yes' is the default
